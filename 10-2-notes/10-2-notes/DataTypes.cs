25d6d63c66b514d40199909c941259529c8e8d6f
Unknown <jcarpenter@student.neumont.edu>
2018-10-02 08:46:51 -0600

    notes

---
+++ 10-2-notes/10-2-notes/DataTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_2_notes
+{
+    class DataTypes
+    {
+        public static void Display()
+        {
+            bool boolean = true;
+            Console.WriteLine("boolean: {0}", boolean);
+        }
+    }
+    
+}
---
+++ 10-2-notes/10-2-notes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_2_notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(int i = 0; i<args.Length; i++)
+            //{
+            //    Console.WriteLine("Args: {0}", args[i]);
+            //}
+            DataTypes.Display();
+            if(args.Contains("red"))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
